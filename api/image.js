3caeb8d69d04e9762a438a12edc4b65ebd49d5cc
sdfgdafsgs <alcash@alcashedu.com>
2025-02-12 00:06:34 +0500

    Create image.js

---
+++ api/image.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+  const userAgent = req.headers['user-agent'] || '';
+  const isFacebookBot = /facebookexternalhit|Facebot/i.test(userAgent);
+
+  // فیک تصویر کا راستہ
+  const fakeImagePath = "/fake-image.jpg";
+  
+  // اصل تصویر کا راستہ
+  const realImagePath = "/real-image.jpg";
+
+  // Facebook کو فیک تصویر بھیجیں
+  if (isFacebookBot) {
+    res.writeHead(302, {
+      Location: `${process.env.VERCEL_URL}${fakeImagePath}`
+    });
+    res.end();
+  } 
+  // عام صارفین کو اصل تصویر بھیجیں
+  else {
+    res.writeHead(302, {
+      Location: `${process.env.VERCEL_URL}${realImagePath}`
+    });
+    res.end();
+  }
+}
